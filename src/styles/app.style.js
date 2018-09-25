073688551b456f9c50ddbb87d17451f504adcd18
Antonette Caldwell <pullmana8@gmail.com>
2018-09-25 17:09:36 -0500

    updated styling

--- src/styles/app.style.js
+++ src/styles/app.style.js
@@ -3,9 +3,9 @@ import style from './style';
 const s = Object.create(style);
 
 s.root = {
-  fontFamily: 'helvetica, sans-serif',
-  fontWeight: '300',
-  fontSize: '16px',
+  fontFamily: 'Lato, sans-serif',
+  fontWeight: '400',
+  fontSize: '15px/1.8',
   letterSpacing: '0.025em',
   padding: '3vh 0 12vh 0',
   width: '500px',
@@ -13,7 +13,8 @@ s.root = {
   // space for vertical scroll bar without creating horizontal scroll bar
   // (if there is padding, the window will scroll horizontally to show the padding)
   maxWidth: 'calc(100vw - 40px)',
-
+  color: '#777',
+  
   // center based on vw to prevent content jump when vertical scroll bar show/hide
   // note: vw/vh include the width of scroll bars. Note that centering using margin auto
   // or % (which doesn't include scroll bars, so changes when scroll bars shown) causes a page jump
